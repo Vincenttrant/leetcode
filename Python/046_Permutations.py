199f6c9726737911f0dc26e3635a91f9c18206fe
Vincent Tran <tranvincent73@gmail.com>
2024-01-15 22:38:43 -0600

    Create 46 Permutations

---
+++ Python/046_Permutations.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def permute(self, nums):
+        def recursive(nums, perm, res):
+            if not nums:
+                res.append(perm[::])
+
+            for i in range(len(nums)):
+                newNums = nums[:i] + nums[i + 1:]
+                perm.append(nums[i])
+                recursive(newNums, perm, res)
+                perm.pop()
+            return res
+
+        return recursive(nums, [], [])
